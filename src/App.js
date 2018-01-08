195921e85d4b33f2432638c29eefa9f3652b1412
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-08 01:35:14 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
