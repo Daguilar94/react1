31916acabdb31ece720d96807d0fc6a54ddf6e8f
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-11 22:07:32 -0500

    Add h1 and p among a div in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+/// Mostrar una lista en base a un arreglo
 
 class App extends Component {
+
   render() {
     return (
       <div>
-        <h1>Mi primera aplicación</h1>
-        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
+import Setup from './Setup';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders one ul tag', () => {
+  const wrapper = render(<App />);
+  expect(wrapper.find('ul').length).toBe(1);
+});
+
+it('renders four li tags', () => {
+  const wrapper = render(<App />);
+  expect(wrapper.find('li').length).toBe(4);
+});
+
+it('rendered the li with correct text', () => {
+  const wrapper = shallow(<App />);
+  const techs = ['Javascript', 'CSS', 'HTLM', 'React']
+  expect(wrapper.find('li').length).toBe(4);
+  wrapper.find('li').forEach(function(node, i) {
+    expect(node.text()).toBe(techs[i]);
+  });
 });
